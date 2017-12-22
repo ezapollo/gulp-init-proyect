36f08993cdcfe2be2179eac9de33821d9185b3a7
Esteban Zenzerovich <estebanz@apolo.local>
2017-12-22 09:17:47 -0300

    2nd commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+// https://css-tricks.com/gulp-for-beginners/
+
+var gulp = require('gulp');
+
+var imagemin = require('gulp-imagemin');
+
+gulp.task('task-name', function () {
+    // Stuff here
+});
