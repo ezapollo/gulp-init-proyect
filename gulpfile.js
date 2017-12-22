abad3ab9462547a1298194575c79a7289f879319
Esteban Zenzerovich <estebanz@apolo.local>
2017-12-22 10:31:28 -0300

    gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,13 @@
-// https://css-tricks.com/gulp-for-beginners/
-
 var gulp = require('gulp');
-var cache = require('gulp-cache');
-var del = require('del');
+var sass = require('gulp-sass');
 
-gulp.task('images', function () {
-    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-        // Caching images that ran through imagemin
-        .pipe(cache(imagemin({
-            interlaced: true
-        })))
-        .pipe(gulp.dest('dist/images'))
+gulp.task('styles', function() {
+    gulp.src('app/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('app/css'))
 });
 
-var imagemin = require('gulp-imagemin');
-
-gulp.task('task-name', function () {
-    // Stuff here
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('app/sass/**/*.scss',['styles']);
 });
-
-gulp.task('fonts', function () {
-    return gulp.src('app/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts'))
-})
-
-
-gulp.task('clean:dist', function () {
-    return del.sync('dist');
-})
-
-
-gulp.task('watch', ['browserSync', 'sass'], function () {
-    // ... watchers
-})
-
-
-gulp.task('build', [`clean`, `sass`, `useref`, `images`, `fonts`], function () {
-    console.log('Building files');
-})
