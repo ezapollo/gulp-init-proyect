c77b68654896d259fbd930e2e49a692c454041a4
Esteban Zenzerovich <estebanz@apolo.local>
2017-12-22 09:25:11 -0300

    gulp build test

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,40 @@
 // https://css-tricks.com/gulp-for-beginners/
 
 var gulp = require('gulp');
+var cache = require('gulp-cache');
+var del = require('del');
+
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+        // Caching images that ran through imagemin
+        .pipe(cache(imagemin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+});
 
 var imagemin = require('gulp-imagemin');
 
 gulp.task('task-name', function () {
     // Stuff here
 });
+
+gulp.task('fonts', function () {
+    return gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+})
+
+
+gulp.task('clean:dist', function () {
+    return del.sync('dist');
+})
+
+
+gulp.task('watch', ['browserSync', 'sass'], function () {
+    // ... watchers
+})
+
+
+gulp.task('build', [`clean`, `sass`, `useref`, `images`, `fonts`], function () {
+    console.log('Building files');
+})
